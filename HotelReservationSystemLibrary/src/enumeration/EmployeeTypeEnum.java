f954ed570b6bda06beb76cc634e50c168a4869ec
pangbrian <e1155284@u.nus.edu>
2024-11-02 00:51:38 +0800

    completed entity class Employee

---
+++ HotelReservationSystemLibrary/src/enumeration/EmployeeTypeEnum.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package enumeration;
+
+/**
+ *
+ * @author brianpang
+ */
+public enum EmployeeTypeEnum {
+    SYSTEM_ADMIN, OPERATION_MANAGER, SALES_MANAGER, GUEST_RELATION_OFFICER
+}
